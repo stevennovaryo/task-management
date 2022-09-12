4463f459332cbe644cdf1a96e9b896fc4c5a0a8b
Insta-x <uttsadajason@outlook.com>
2022-09-12 21:46:21 +0700

    Catalog items data loaded

--- katalog/views.py
+++ katalog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 def show_katalog(request):
-    return render(request, "katalog.html")
+    data_catalog_items = CatalogItem.objects.all()
+    context = {
+        'item_list' : data_catalog_items,
+        'name' : 'Uttsada Jason',
+        'npm' : '2106629976',
+    }
+    return render(request, "katalog.html", context)
