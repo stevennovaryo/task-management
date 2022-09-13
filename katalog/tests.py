986412f3ee6d36af240bfd0a4c12d6b80389b090
Insta-x <uttsadajason@outlook.com>
2022-09-13 09:14:51 +0700

    added simple testing

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from katalog.models import CatalogItem
 
-# Create your tests here.
+class KatalogTests(TestCase):
+    def test_create_catalog_item(self):
+        item = CatalogItem.objects.create(
+            item_name = "ASUS ROG Zephyrus G15", 
+            item_price = 26149000, 
+            item_stock = 1, 
+            description = "Ryzen 6800HS & RTX 3060", 
+            rating = 5, 
+            item_url = "https://www.tokopedia.com/nvidiageforcelt/asus-rog-zephyrus-g15-ga503rm-geforce-rtx-3060-ryzen-7-6800hs-unit"
+        )
+        self.assertEquals(item.item_name, "ASUS ROG Zephyrus G15")
+        self.assertEquals(item.item_price, 26149000)
+        self.assertEquals(item.item_stock, 1)
+        self.assertEquals(item.description, "Ryzen 6800HS & RTX 3060")
+        self.assertEquals(item.rating, 5)
+        self.assertEquals(item.item_url, "https://www.tokopedia.com/nvidiageforcelt/asus-rog-zephyrus-g15-ga503rm-geforce-rtx-3060-ryzen-7-6800hs-unit")
