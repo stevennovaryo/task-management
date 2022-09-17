60b2917dca359af73f31d7226627fd92801c78db
Insta-x <uttsadajason@outlook.com>
2022-09-17 18:05:51 +0700

    mywatchlist local page

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+def show_watchlist(request):
+    data_watchlist_movies = MyWatchList.objects.all()
+    context = {
+        'movie_list' : data_watchlist_movies,
+        'name' : 'Uttsada Jason',
+        'npm' : '2106629976',
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data_watchlist_movies = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data_watchlist_movies), content_type="application/xml")
+
+def show_json(request):
+    data_watchlist_movies = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data_watchlist_movies), content_type="application/json")
