32d49ca55a31928b55aa6603db1862f7712ce9a2
Insta-x <uttsadajason@outlook.com>
2022-09-18 21:53:14 +0700

    tests passed

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from http import client
+from multiprocessing.connection import Client
+from pydoc import cli
+from urllib import response
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class MyWatchListTests(TestCase):
+    def test_html_status(self): 
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_watchlist"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_xml_status(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_xml"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_json_status(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_json"))
+        self.assertEqual(response.status_code, 200)
